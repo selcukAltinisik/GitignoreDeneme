26f2667af4008edf3c325d25a22794ba11728e5a
selcukAltinisik <selcukaltin3544@gmail.com>
2022-09-06 02:50:36 +0300

    Master Commiti

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -2,6 +2,7 @@ public class Gitignore {
     public static void main(String[] args) {
 
         System.out.println("Git ignore deneme 1");
+        System.out.println("Git ignore deneme 2");
 
     }
 }
